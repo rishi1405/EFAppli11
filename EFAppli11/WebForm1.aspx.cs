c04e8481ed205433c6aa8cc38c83ef24cffcb6a3
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 14:44:21 +0530

    EFAppli11

---
+++ EFAppli11/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EFAppli11
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EmplDBContext employeeDBContext = new EmplDBContext();
+            GridView1.DataSource = employeeDBContext.Empls.Select(emp => new
+            {
+                EmployeeName = emp.EmployeeName,
+                ManagerName = emp.Manager == null ? "Super Boss"
+                                : emp.Manager.EmployeeName
+            }).ToList();
+            GridView1.DataBind();
+        }
+    }
+}
